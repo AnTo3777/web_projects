7d8d0ba1431206adcd583833dd13b43b712c491c
AnTo3777 <an.moutier123@gmail.com>
2022-04-15 13:43:50 +0200

    alpha v2

--- app.js
+++
@@ -1,8 +0,0 @@
-function change_score(){
-    score = parseInt(Math.random()*10)
-    affichage.innerHTML = score
-
-    localStorage.setItem('score',score);
-}
-
-console.log(localStorage.getItem('score'))
